a016184e3adee7dcd495736f5e33f40fced95adf
Makis Otman <makisotman@gmail.com>
2014-11-18 13:54:38 +0000

    Replace magic numbers with symbolic constants

--- java/videostore/src/Customer.java
+++ java/videostore/src/Customer.java
@@ -2,6 +2,11 @@ import java.util.Vector;
 import java.util.Enumeration;
 
 public class Customer {
+  private static final int STARTING_AMOUNT = 0;
+  private static final int TWO_DAYS = 2;
+  private static final int THREE_DAYS = 3;
+  private static final double ONE_AND_HALF_DAY = 1.5;
+  private static final int ONE_DAY = 1;
   private String name;
   private Vector<Rental> rentals = new Vector<Rental>();
 
@@ -18,36 +23,36 @@ public class Customer {
   }
 
   public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
+    double totalAmount = STARTING_AMOUNT;
+    int frequentRenterPoints = STARTING_AMOUNT;
     Enumeration rentals = this.rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
 
     while (rentals.hasMoreElements()) {
-      double thisAmount = 0;
+      double thisAmount = STARTING_AMOUNT;
       Rental each = (Rental) rentals.nextElement();
 
       // determines the amount for each line
       switch (each.getMovie().getPriceCode()) {
         case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
+          thisAmount += TWO_DAYS;
+          if (each.getDaysRented() > TWO_DAYS)
+            thisAmount += (each.getDaysRented() - TWO_DAYS) * ONE_AND_HALF_DAY;
           break;
         case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
+          thisAmount += each.getDaysRented() * THREE_DAYS;
           break;
         case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
+          thisAmount += ONE_AND_HALF_DAY;
+          if (each.getDaysRented() > THREE_DAYS)
+            thisAmount += (each.getDaysRented() - THREE_DAYS) * ONE_AND_HALF_DAY;
           break;
       }
 
       frequentRenterPoints++;
 
       if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE
-        && each.getDaysRented() > 1)
+        && each.getDaysRented() > ONE_DAY)
         frequentRenterPoints++;
 
       result += "\t" + each.getMovie().getTitle() + "\t"
--- java/videostore/src/VideoStoreTest.java
+++ java/videostore/src/VideoStoreTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class VideoStoreTest {
+
+  private static final int THREE_DAYS = 3;
+  private static final int ONE_DAY = 1;
+  private static final int TWO_DAYS = 2;
+
   @Before
   public void setUp() {
     customer = new Customer("Fred");
@@ -11,28 +16,28 @@ public class VideoStoreTest {
 
   @Test
   public void singleNewReleaseStatement() {
-    customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+    customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), THREE_DAYS));
     assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
   }
 
   @Test
   public void dualNewReleaseStatement() {
-    customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
-    customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
+    customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), THREE_DAYS));
+    customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), THREE_DAYS));
     assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement());
   }
 
   @Test
   public void singleChildrensStatement() {
-    customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
+    customer.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), THREE_DAYS));
     assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
   }
 
   @Test
   public void multipleRegularStatement() {
-    customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
-    customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
-    customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+    customer.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), ONE_DAY));
+    customer.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), TWO_DAYS));
+    customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), THREE_DAYS));
 
     assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement());
   }
