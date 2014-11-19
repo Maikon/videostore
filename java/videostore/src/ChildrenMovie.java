000377e6ab61744092564ad630cd6036d56086ec
Makis Otman <makisotman@gmail.com>
2014-11-19 15:56:26 +0000

    Remove constants to simplify getAmount() in subclasses

--- java/videostore/src/ChildrenMovie.java
+++ java/videostore/src/ChildrenMovie.java
@@ -1,18 +1,14 @@
 public class ChildrenMovie extends Movie {
 
-  private static final double ONE_AND_HALF_DAY = 1.5;
-  private static final int THREE_DAYS = 3;
-
   public ChildrenMovie(String title) {
     super(title);
   }
 
   @Override
   public double getAmount(int daysRented) {
-    double amount = ONE_AND_HALF_DAY;
-    if (daysRented > THREE_DAYS) {
-      amount += (daysRented - THREE_DAYS) * amount;
+    if (daysRented > 3) {
+      return 1.5 + (daysRented - 3) * 1.5;
     }
-    return amount;
+    return 1.5;
   }
 }
--- java/videostore/src/RegularMovie.java
+++ java/videostore/src/RegularMovie.java
@@ -1,18 +1,14 @@
 public class RegularMovie extends Movie {
 
-  private final double TWO_DAYS = 2.0;
-  private final double ONE_AND_HALF_DAY = 1.5;
-
   public RegularMovie(String title) {
     super(title);
   }
 
   @Override
   public double getAmount(int daysRented) {
-    double amount = TWO_DAYS;
-    if (daysRented > TWO_DAYS) {
-      amount += (daysRented - TWO_DAYS) * ONE_AND_HALF_DAY;
+    if (daysRented > 2) {
+      return 2 + (daysRented - 2) * 1.5;
     }
-    return amount;
+    return 2;
   }
 }
