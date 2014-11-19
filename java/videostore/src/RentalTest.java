bdaa23d6283fa1d6792b526d7f274979f7d02200
Makis Otman <makisotman@gmail.com>
2014-11-19 16:06:50 +0000

    Refactor Rental tests

--- java/videostore/src/RentalTest.java
+++ java/videostore/src/RentalTest.java
@@ -5,54 +5,48 @@ import static org.junit.Assert.assertThat;
 
 public class RentalTest {
 
-  public static final int ONE_DAY_RENTED = 1;
-  public static final int THREE_DAYS_RENTED = 3;
-  public static final int SIX_DAYS_RENTED = 6;
+  private static final int ONE_DAY_RENTED = 1;
+  private static final int THREE_DAYS_RENTED = 3;
+  private static final int SIX_DAYS_RENTED = 6;
+  private static final RegularMovie REGULAR_MOVIE = new RegularMovie("Gravity");
+  private static final NewReleaseMovie NEW_RELEASE_MOVIE = new NewReleaseMovie("Interstellar");
+  private static final ChildrenMovie CHILDREN_MOVIE = new ChildrenMovie("Winnie the Pooh");
 
   @Test
   public void getAmountOwedForRegularMovie() {
-    RegularMovie movie = new RegularMovie("Gravity");
-    Rental rental = new Rental(movie, ONE_DAY_RENTED);
+    Rental rental = new Rental(REGULAR_MOVIE, ONE_DAY_RENTED);
     assertThat(rental.getAmountForMovie(), is(2.0));
   }
 
   @Test
   public void getAmountOwedForRegularMovieLongerThanTwoDays() {
-    RegularMovie movie = new RegularMovie("Gravity");
-    Rental rental = new Rental(movie, THREE_DAYS_RENTED);
+    Rental rental = new Rental(REGULAR_MOVIE, THREE_DAYS_RENTED);
     assertThat(rental.getAmountForMovie(), is(3.5));
   }
 
   @Test
   public void getAmountOwedForNewReleaseMovie() {
-    NewReleaseMovie movie = new NewReleaseMovie("Interstellar");
-    Rental rental = new Rental(movie, ONE_DAY_RENTED);
+    Rental rental = new Rental(NEW_RELEASE_MOVIE, ONE_DAY_RENTED);
     assertThat(rental.getAmountForMovie(), is(3.0));
   }
 
   @Test
   public void getAmountOwedForChildrenMovie() {
-    ChildrenMovie movie = new ChildrenMovie("Winnie the Pooh");
-    Rental rental = new Rental(movie, ONE_DAY_RENTED);
+    Rental rental = new Rental(CHILDREN_MOVIE, ONE_DAY_RENTED);
     assertThat(rental.getAmountForMovie(), is(1.5));
   }
 
   @Test
   public void getAmountOwedForChildrenMovieLongerThanThreeDays() {
-    ChildrenMovie movie = new ChildrenMovie("Winnie the Pooh");
-    Rental rental = new Rental(movie, SIX_DAYS_RENTED);
+    Rental rental = new Rental(CHILDREN_MOVIE, SIX_DAYS_RENTED);
     assertThat(rental.getAmountForMovie(), is(6.0));
   }
 
   @Test
   public void getFrequentPointsForMovie() {
-    RegularMovie regularMovie = new RegularMovie("Gravity");
-    ChildrenMovie childrenMovie = new ChildrenMovie("Winnie the Pooh");
-    NewReleaseMovie newReleaseMovie = new NewReleaseMovie("Interstellar");
-
-    Rental rentalOne = new Rental(regularMovie, ONE_DAY_RENTED);
-    Rental rentalTwo = new Rental(childrenMovie, ONE_DAY_RENTED);
-    Rental rentalThree = new Rental(newReleaseMovie, ONE_DAY_RENTED);
+    Rental rentalOne = new Rental(REGULAR_MOVIE, ONE_DAY_RENTED);
+    Rental rentalTwo = new Rental(CHILDREN_MOVIE, ONE_DAY_RENTED);
+    Rental rentalThree = new Rental(NEW_RELEASE_MOVIE, ONE_DAY_RENTED);
 
     assertThat(rentalOne.getFrequentPointsForMovie(), is(1));
     assertThat(rentalTwo.getFrequentPointsForMovie(), is(1));
@@ -61,8 +55,7 @@ public class RentalTest {
 
   @Test
   public void getFrequentPointsForNewReleaseMovieLongerThanThreeDays() {
-    NewReleaseMovie movie = new NewReleaseMovie("Interstellar");
-    Rental rental = new Rental(movie, SIX_DAYS_RENTED);
+    Rental rental = new Rental(NEW_RELEASE_MOVIE, SIX_DAYS_RENTED);
 
     assertThat(rental.getFrequentPointsForMovie(), is(2));
   }
